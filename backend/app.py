a1229a45366d872752562b01639f9358f5e3b891
Meah Tahmeed Ahmed <tahmeed121212@gmail.com>
2024-11-17 11:05:02 -0600

    t2

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import json
-import data_helper
 
 app = Flask(__name__)
 CORS(app)  
@@ -14,9 +13,6 @@ DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "./data.json")
 UPLOAD_FOLDER = './uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-data_helper.send_each("../machine-learning/normalized-data")
 
 def load_pipe():
     with open(DB_PATH, "r") as f:
@@ -33,9 +29,6 @@ def echo():
     user_input = data.get('input')
     return jsonify({"output": user_input})
 
-@app.route('/api/data', methods=['POST'])
-def data():
-    return jsonify(data_helper.send_each("../machine-learning/normalized-data"))
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -50,8 +43,6 @@ def upload_file():
     if file and file.filename.endswith('.csv'):
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filepath)
-        data_helper.analyze_data("./uploads/Fearless_709H-10_31-11_07.csv", "/Users/sifatislam/Downloads/PROJECTS/Hack UTD/HackUTD_EOGxPNC/machine-learning/model_95.pkl")
-        print("data helper called")
         return {"message": "File successfully uploaded"}, 200
     else:
         return {"message": "Invalid file type. Only CSV files are allowed."}, 400
